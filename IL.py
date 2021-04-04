5f26d80101ad41713f648b76307cdbc8e04f5aba
106000135 <61a5t0@gmail.com>
2021-04-05 01:12:32 +0800

    Fix prev action bug in IL

--- IL.py
+++ IL.py
@@ -106,7 +106,8 @@ def agent(obs_dict, config_dict):
     X_test = X_test.reshape(-1,7,11,17) # channel last.
     
     # avoid suicide
-    obstacles = X_test[:,:,:,[8,9,10,11,12]].max(axis=3) - X_test[:,:,:,[4,5,6,7]].max(axis=3) # body + opposite_side - my tail
+    # obstacles = X_test[:,:,:,[8,9,10,11,12]].max(axis=3) - X_test[:,:,:,[4,5,6,7]].max(axis=3) # body + opposite_side - my tail
+    obstacles = X_test[:,:,:,[8,9,10,11]].max(axis=3) - X_test[:,:,:,[4,5,6,7]].max(axis=3) + X_test[:,:,:,[12]].max(axis=3)
     obstacles = np.array([obstacles[0,2,5], obstacles[0,4,5], obstacles[0,3,4], obstacles[0,3,6]])
     
     y_pred = model.predict(X_test) - obstacles
